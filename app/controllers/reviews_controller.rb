7dac37dc01254bb98e7ce09291f286d20283a800
Dalynn4 <keith_adams007@yahoo.com>
2018-08-13 17:11:59 -0400

    connected rails api with react, hopefully

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+
+  def index
+    render(
+      status: 200,
+      json: Review.all
+    )
+  end
+
+  def new
+  end 
+
+  def show
+  end
+  
 end
