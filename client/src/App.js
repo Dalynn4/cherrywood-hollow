ff830dd494f29802a066485bd5da507dc60f31a3
Dalynn4 <keith_adams007@yahoo.com>
2018-08-14 19:15:57 -0400

    working on reviews form to see if api is working.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+
+  
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
+          <h1 className="App-title">Cherrywood Hollow</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2 className="App-intro">
+          Review Form
+          <form>
+            <p>Name:</p>
+            <input value="user_name" />
+            <p>Review or Comment:</p>
+            <input value="content" />
+            <input onClick={this.submitReview} type="submit" />
+            </form>
+        </h2>
       </div>
     );
   }
