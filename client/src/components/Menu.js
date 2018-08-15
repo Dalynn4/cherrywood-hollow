e6ed595caf2d0ff8a744b9c36a56074c78441ce7
Dalynn4 <keith_adams007@yahoo.com>
2018-08-14 21:05:03 -0400

    set up menu items

---
+++ client/src/components/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+      <ul className="Menu">
+        <li><a href="/">Home</a></li>
+        <li><a href="/trailer">Trailer</a></li>
+        <li><a href="/reviews">Reviews</a></li>
+      </ul>
+      </div>
+  )
+}
+
+export default Menu
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
+import Menu from './components/Menu'
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
