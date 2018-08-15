039b23d9e165357f7ef9b2fe91a53dee068ff18c
Dalynn4 <keith_adams007@yahoo.com>
2018-08-15 19:27:54 -0400

    addstatetoprops for reviews.js

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,31 +1,37 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Reviews extends Component {
 
-  state = {
-    userName: "Name",
-    content: "Review or Comment",
-    reviews: []
-  }
+  //state = {
+  // userName: "Name",
+  //content: "Review or Comment",
+  //  reviews: []
+  //}
 
-  componentDidMount() {
-    fetch('http://localhost:3001/reviews')
-    .then(response => response.json())
-    .then(data => {
-      this.setState ({
-        reviews: data
-      })
-    })
-  }
+ // componentDidMount() {
+ //   fetch('http://localhost:3001/reviews')
+ //   .then(response => response.json())
+ //  .then(data => {
+  //    this.setState ({
+  //      reviews: data
+  //    })
+  //  })
+  //}
 
   render() {
     return (
       <ol>
-      {this.state.reviews.map(review =><li key={review.id}>Name:{review.user_name} Content:{review.content}</li>)}
+      {this.props.reviews.map(review =><li key={review.id}>Name:{review.user_name} Content:{review.content}</li>)}
       </ol>
     )
   }
-
 }
 
-export default Reviews
+const mapStateToProps = state => {
+  return {
+    reviews: state.reviews
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Reviews)
