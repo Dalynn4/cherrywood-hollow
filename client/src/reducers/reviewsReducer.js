eeea921f5e0f028094c51df769bede8df4963489
Dalynn4 <keith_adams007@yahoo.com>
2018-08-15 19:15:26 -0400

    working on actions and reducers

--- client/src/reducers/reviewsReducer.js
+++ client/src/reducers/reviewsReducer.js
@@ -1,8 +1,28 @@
+import fetchReviews from '../actions/fetchReviews'
+import newReview from '../actions/newReview'
 
+const intialState = {
+  userName: "Name",
+  content: "Review or Comment",
+  reviews: []
+}
 
-
-const reviewsReducer = () => {
-  console.log("ape")
+const reviewsReducer = (state = intialState, action) => {
+  switch (action.type) {
+    case "FETCH_REVIEWS_START": {
+      return {...state, reviews: "Loading Reviews"}
+      break;
+    }
+    case "REVIEWS_FETCHED": {
+      return {...state, reviews: action.reviews}
+      break;
+    }
+    case "ADD_REVIEW": {
+      return {...state, reviews: state.reviews.concat(action.review)}
+    }
+    default:
+    return state
+  }
 }
 
 export default reviewsReducer
