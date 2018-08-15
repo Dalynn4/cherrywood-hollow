0c2a834a7f70f1827e85c4e70b63257c28e2150b
Dalynn4 <keith_adams007@yahoo.com>
2018-08-14 23:04:36 -0400

    added trailer component

---
+++ client/src/components/Trailer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Trailer = () => {
+  return(
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  )
+}
+
+export default Trailer
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
-import Menu from './components/Menu'
+import Menu from './components/Menu';
+import Trailer from './components/Trailer'
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ ReactDOM.render((
   <React.Fragment>
     <Menu />
     <Route path="/" component={App} />
+    <Route path="/trailer" component={Trailer} />
   </React.Fragment>
 </Router>),
 document.getElementById('root'));
