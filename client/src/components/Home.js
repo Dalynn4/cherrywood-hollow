c6b1189a3c3323f7213a5ce4f56575e6dfb84a1f
Dalynn4 <keith_adams007@yahoo.com>
2018-08-16 00:10:47 -0400

    workin on styling and profiling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -12,11 +12,11 @@ class Home extends Component {
         </style>
         <h1 className="title">Cherrywood Hollow</h1>
         <h4 className="body">Experience the Party Game Phenomenom, Now on Your Phone!</h4>
-        <ol className="hooks">
-          <li>Play the game like never before!</li>
-          <li>Support for up to 20 players per game!</li>
-          <li>Save your settings for less downtime in a run of games!</li>
-        </ol>
+          <ol className="hooks">
+            <li>Play the game like never before!</li>
+            <li>Support for up to 20 players per game!</li>
+            <li>Save your settings for less downtime in a run of games!</li>
+          </ol>
       </div>
     );
   }
