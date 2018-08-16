b7a37305d638a9a810c01e08901d0beed7dd906c
Dalynn4 <keith_adams007@yahoo.com>
2018-08-15 23:59:12 -0400

    Home styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import '../css/Home.css';
 
 
 class Home extends Component {
@@ -7,9 +7,16 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <header className="header">
-          <h1 className="title">Cherrywood Hollow</h1>
-        </header>
+        <style>
+          @import url('https://fonts.googleapis.com/css?family=Playfair+Display+SC');
+        </style>
+        <h1 className="title">Cherrywood Hollow</h1>
+        <h4 className="body">Experience the Party Game Phenomenom, Now on Your Phone!</h4>
+        <ol className="hooks">
+          <li>Play the game like never before!</li>
+          <li>Support for up to 20 players per game!</li>
+          <li>Save your settings for less downtime in a run of games!</li>
+        </ol>
       </div>
     );
   }
