4c7eb150d69eef27ec71320d8ecd86a0abe17f43
Dalynn4 <keith_adams007@yahoo.com>
2018-08-16 19:19:28 -0400

    trailer styling

--- client/src/components/Trailer.js
+++ client/src/components/Trailer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import '../css/Trailer.css'
 
 const Trailer = () => {
   return(
-    <iframe title="Welcome to Cherrywood Hollow" width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+    <div className="trailer">
+      <style>
+        @import url('https://fonts.googleapis.com/css?family=Playfair+Display+SC');
+      </style>
+      <h1 className="title">Trailer</h1>
+    <iframe  title="Welcome to Cherrywood Hollow" width="560" height="315"  src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+    </div>
   )
 }
 
